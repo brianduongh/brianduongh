212886b3596199f3d309a1bceb57f4d7b7dae972
Brian Duong <brianduongh@gmail.com>
2020-07-13 14:48:47 -0500

    Add babel/typescript/webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    module: {
+        rules: [
+          {
+            test: /\.(t|j)sx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+          }
+        ]
+      },
+      resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx']
+      }
+}
